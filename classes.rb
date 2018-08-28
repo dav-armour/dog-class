b707c897a9844049e7a6d3232032a77022209bec
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-08-28 22:03:27 +1000

    make display walks

--- classes.rb
+++ classes.rb
@@ -13,33 +13,20 @@ class Dog
   end
 
   def walk(location, distance)
-    @walks << { :location => location, :distance => distance, :time => Time.now }
+    @walks << { :location => location, :distance => distance, :time => Time.now.strftime("%d/%m/%Y %I:%M%p") }
     self
   end
 
-  def display_walks(location = nil)
-    counter = 0
-    distance = 0
-    @walks.each do |walkHash|
-      if walkHash[:location] == location || location == nil
-        counter += 1
-        distance += walkHash[:distance]
-      end
+  def display_walks
+    walk_count = @walks.length
+    puts "I have been for #{walk_count} walks today"
+    @walks.each do |walk_hash|
+      output = "Time: #{walk_hash[:time]}".ljust(30)
+      output += "Location: #{walk_hash[:location]}".ljust(25)
+      output += "Distance: #{walk_hash[:distance]}km"
+      puts output
     end
-    output = "I have been for #{counter} walks and traveled #{distance}km"
-    output += " at #{location.capitalize}" if location
-    puts output
-    self
   end
-
-  # def display_walks2(location = nil)
-  #   counter = @walks.count { |h| h[:location] == location }
-  #   distance = @walks.sum { |h| h[:location] == location ? h[:distance] : 0 }
-  #   output = "I have been for #{counter} walks and traveled #{distance}km"
-  #   output += " at #{location.capitalize}" if location
-  #   puts output
-  #   self
-  # end
 
   def total_distance
     @walks.sum { |h| h[:distance] }
@@ -53,11 +40,5 @@ puts oldDog.location
 oldDog.walk('sydney', 20)
 oldDog.walk('sydney', 5)
 oldDog.walk('brisbane', 30)
-oldDog.display_walks('sydney')
-oldDog.display_walks('brisbane')
 oldDog.display_walks
-# oldDog.display_walks2('sydney')
-# oldDog.display_walks2('brisbane')
-# oldDog.display_walks2
 puts "Total Distance: #{oldDog.total_distance}km"
-# oldDog.walk.walk.display_walks
